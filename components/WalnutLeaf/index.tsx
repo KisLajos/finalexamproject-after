c1fb9f5f01750e4c43ccb885e86d4ff665a2119c
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-05-23 23:53:20 +0200

    Added reusable WalnutLeaf component

--- components/WalnutLeaf/index.tsx
+++ components/WalnutLeaf/index.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image'
 import React from 'react'
+import styles from './styles.module.scss'
 
 export interface WalnutLeafProps {
     width: number,
-    height: number
+    height: number,
+    colour: string
 }
 
-export default function WalnutLeaf({width, height} : WalnutLeafProps) {
+export default function WalnutLeaf({width, height, colour} : WalnutLeafProps) {
   return (
-    <div>
-        <Image src={"/walnutleaf.svg"} alt='Diólevél Dekorműhely' width={width} height={height}/>
+    <div className={styles.walnutleafContainer}>
+        <Image src={`/walnutleaf-${colour}.svg`} alt='Diólevél Dekorműhely' width={width} height={height}/>
     </div>
   )
 }
