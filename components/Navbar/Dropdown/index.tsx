5d42240d56fc6214b64d6943d91881245e8d0908
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-05-23 23:56:17 +0200

    Key fix

--- components/Navbar/Dropdown/index.tsx
+++ components/Navbar/Dropdown/index.tsx
@@ -23,8 +23,8 @@ export const DropdownMenu = ({categorychildren, parentname} : DropdownProps) => 
     return (
         <ul className={navbarstyles.dropdown_content}>
                 {categorychildren.map((categorychild) => (
-                    <div className={styles.dropdowncontentContainer}>
-                        <li key={categorychild.term_id} className={`${navlinkstyles.nav_link} ${navbarstyles.nav_link__child}`}>
+                    <div key={categorychild.term_id} className={styles.dropdowncontentContainer}>
+                        <li className={`${navlinkstyles.nav_link} ${navbarstyles.nav_link__child}`}>
                             <WalnutLeaf colour="lavender" width={20} height={20}/>
                             <Link href={`/${parentname}/${categorychild.slug}`}>{categorychild.name}</Link>
                         </li>
