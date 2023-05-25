77070565ee333105ca7e04c8f9c4ea2dd1f835f8
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-05-26 00:13:33 +0200

    How we work styling

--- components/HowWeWork/index.tsx
+++ components/HowWeWork/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import PageCard from '../PageCard'
+import SectionHeader from '../SectionHeader'
 import styles from './styles.module.scss'
-import WalnutLeaf from '../WalnutLeaf'
 
 export default function HowWeWork() {
   return (
     <div className={styles.HowWeWorkContainer}>
-      <div className={styles.HowWeWorkContainerHeader}>
-        <WalnutLeaf colour='logocolours' width={50} height={50}/>
-        <h1>Így dolgozunk</h1>
-      </div>
+      <SectionHeader headertext="Így dolgozunk"/>
       <div className={styles.HowWeWorkCardsContainer}>
         <PageCard type="howwework" title="Vibe" links={[""]} bodytext={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus dicta perspiciatis ipsam possimus harum. Quos, provident inventore facilis eaque hic perferendis necessitatibus accusamus perspiciatis nemo, ipsam asperiores beatae omnis dolore."}/>
         <PageCard type="howwework" title="Tervezés" links={[""]} bodytext={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus dicta perspiciatis ipsam possimus harum. Quos, provident inventore facilis eaque hic perferendis necessitatibus accusamus perspiciatis nemo, ipsam asperiores beatae omnis dolore."}/>
