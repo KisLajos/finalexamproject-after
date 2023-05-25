3f4064a7ac3edfbcd34812c6695a33127807b49e
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-05-26 00:12:56 +0200

    Review styling

--- components/Reviews/index.tsx
+++ components/Reviews/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReviewSlider from './ReviewSlider'
 import { getReviews } from '@/utils/wordpressfunctions';
+import styles from './styles.module.scss'
+import SectionHeader from '../SectionHeader';
 
 export default async function Reviews() {
   const reviews = await getReviews();
 
   return (
-    <ReviewSlider reviews={reviews} />
+    <div>
+      <SectionHeader headertext="Visszajelzések"/>
+      <ReviewSlider reviews={reviews} />
+    </div>
   )
 }
