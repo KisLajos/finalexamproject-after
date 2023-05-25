86a2fc9d5a7a3b68948c5a55e3eda2c94c49feef
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-05-26 00:13:19 +0200

    About Us styling

--- components/AboutUs/index.tsx
+++ components/AboutUs/index.tsx
@@ -3,21 +3,25 @@ import styles from './styles.module.scss'
 import Image from 'next/image'
 import parse from 'html-react-parser'
 import { getPageDataBySlug } from '@/utils/wordpressfunctions'
+import SectionHeader from '../SectionHeader'
 
 export default async function AboutUs() {
 const page = await getPageDataBySlug("rolunk")
 
   return (
-    <div className={styles.aboutusContainer}>
-        <Image src={page.acf.about_us_image.url} alt="Flóra & Balázs" width={page.acf.about_us_image.width} height={page.acf.about_us_image.height}/>
-        <div className={styles.aboutusText}>
-            <p>
-                {parse(page.acf.text_block_1)}
-            </p>
-            <p>
-                {parse(page.acf.text_block_2)}
-            </p>
-        </div>
+    <div>
+      <SectionHeader headertext="Rólunk"/>
+      <div className={styles.aboutusContainer}>
+          <Image src={page.acf.about_us_image.url} alt="Flóra & Balázs" width={page.acf.about_us_image.width} height={page.acf.about_us_image.height}/>
+          <div className={styles.aboutusText}>
+              <p>
+                  {parse(page.acf.text_block_1)}
+              </p>
+              <p>
+                  {parse(page.acf.text_block_2)}
+              </p>
+          </div>
+      </div>
     </div>
   )
 }
