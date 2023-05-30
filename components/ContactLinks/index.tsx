c642879edee797051cfa1442a71cc10fcfa90b69
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-05-30 22:54:13 +0200

    ContactLinks styling

--- components/ContactLinks/index.tsx
+++ components/ContactLinks/index.tsx
@@ -1,4 +1,4 @@
-import { faPhone, faEnvelope, faLocationPin } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
@@ -9,7 +9,7 @@ export default function ContactLinks() {
     <div className={styles.contactLinks}>
       <span><FontAwesomeIcon icon={faEnvelope} style={{color: "#f3e4e2", width: "20px"}} />wagner.flora@prsc.hu</span>
       <span><FontAwesomeIcon icon={faPhone} style={{color: "#f3e4e2", width: "20px"}} /> +36 70 609 7223</span>
-      <span><FontAwesomeIcon icon={faLocationPin} style={{color: "#f3e4e2", width: "20px"}} />2330 Dunaharaszti, Fő út 207.</span>
+      <span><FontAwesomeIcon icon={faLocationDot} style={{color: "#f3e4e2", width: "20px"}} />2330 Dunaharaszti, Fő út 207.</span>
       <span><FontAwesomeIcon icon={faFacebook} style={{color: "#f3e4e2", width: "20px"}}/>@dioleveldekor</span>
       <span><FontAwesomeIcon icon={faInstagram} style={{color: "#f3e4e2", width: "20px"}}/> @diolevel_dekormuhely</span>
     </div>
