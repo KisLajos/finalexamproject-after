8992d9017d5bfa3eeb664eab12113403be8049c0
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-06-04 23:08:08 +0200

    Added logo to navbar

--- components/Navbar/NavlinkContainer/index.tsx
+++ components/Navbar/NavlinkContainer/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Category } from '..'
-import navbarstyles from '../styles.module.scss'
 import Link from 'next/link'
+import Image from 'next/image'
+import { Category } from '..'
 import Navlink from '../Navlink'
+import navbarstyles from '../styles.module.scss'
 import styles from './styles.module.scss'
 
 export interface NavlinkContainerProps {
@@ -12,9 +13,11 @@ export interface NavlinkContainerProps {
 export default function NavlinkContainer({categories} : NavlinkContainerProps) {
   //console.log()
   return (
-    <ul className={navbarstyles.nav_links + " " + styles.testclass}>
-        <li>
-          <Link href={"/"}>Főoldal</Link>
+    <ul className={navbarstyles.nav_links}>
+        <li className={styles.navbarLogo}>
+          <Link href={"/"}>
+            <Image src={'/navbarlogo.svg'} alt="Diólevél Dekorműhely" width={250} height={250}/>
+          </Link>
         </li>
         {categories.map((category:Category) => (
               category.acf.show_on_navbar ?
