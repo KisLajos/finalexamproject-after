316c818fc9568362aff0c9b9e77f8535b95379ec
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-06-04 19:16:54 +0200

    Footerlinks fix

--- components/Footer/FooterLinks/index.tsx
+++ components/Footer/FooterLinks/index.tsx
@@ -1,12 +1,13 @@
 import { getCategories } from '@/components/Navbar'
 import React from 'react'
 import FooterlinkContainer from './FooterlinkContainer'
+import styles from './styles.module.scss'
 
 export default async function FooterLinks() {
   const resCategories = await getCategories()
 
   return (
-    <div>
+    <div className={styles.footerLinksContainer}>
       <FooterlinkContainer categories={resCategories}/>
     </div>
   )
