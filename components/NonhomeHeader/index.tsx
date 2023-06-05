2844d3e31133ff31dadf208a6d99225e80571723
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-06-05 17:42:30 +0200

    Adding local font to headers

--- components/NonhomeHeader/index.tsx
+++ components/NonhomeHeader/index.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 import WalnutLeaf from '../WalnutLeaf'
 import parse from 'html-react-parser'
 import styles from './styles.module.scss'
+import localFont from 'next/font/local';
+ 
+// Font files can be colocated inside of `app`
+const BuenardFont = localFont({
+  src: '../../app/fonts/Buenard-Regular.ttf',
+  display: 'swap',
+});
 
 export interface NonHomeHeaderProps {
     content: string
@@ -11,7 +18,7 @@ export default function NonHomeHeader({content}: NonHomeHeaderProps) {
   return (
     <div className={styles.headerContainer}>
         <WalnutLeaf colour="logocolours" width={40} height={40} />
-        <h1>{parse(content)}</h1>
+        <h1 className={BuenardFont.className}>{parse(content)}</h1>
     </div>
   )
 }
