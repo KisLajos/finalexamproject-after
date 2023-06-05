aa87ca02a9541b85003fcec12efc1c7c30c3236d
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-06-05 20:59:04 +0200

    Link fix

--- components/ContactLinks/index.tsx
+++ components/ContactLinks/index.tsx
@@ -10,8 +10,8 @@ export default function ContactLinks() {
       <a href="mailto:wagner.flora@prsc.hu"><FontAwesomeIcon icon={faEnvelope} style={{color: "#f3e4e2", width: "20px"}} />wagner.flora@prsc.hu</a>
       <a href="tel:+36706097223"><FontAwesomeIcon icon={faPhone} style={{color: "#f3e4e2", width: "20px"}} /> +36 70 609 7223</a>
       <span><FontAwesomeIcon icon={faLocationDot} style={{color: "#f3e4e2", width: "20px"}} />2330 Dunaharaszti, Fő út 207.</span>
-      <a href='https://www.instagram.com/diolevel_dekormuhely/'><FontAwesomeIcon icon={faFacebook} style={{color: "#f3e4e2", width: "20px"}}/>@dioleveldekor</a>
-      <a href='https://www.facebook.com/dioleveldekor'><FontAwesomeIcon icon={faInstagram} style={{color: "#f3e4e2", width: "20px"}}/> @diolevel_dekormuhely</a>
+      <a href='https://www.facebook.com/dioleveldekor' target='_blank'><FontAwesomeIcon icon={faFacebook} style={{color: "#f3e4e2", width: "20px"}}/>@dioleveldekor</a>
+      <a href='https://www.instagram.com/diolevel_dekormuhely/' target='_blank'><FontAwesomeIcon icon={faInstagram} style={{color: "#f3e4e2", width: "20px"}}/> @diolevel_dekormuhely</a>
     </div>
   )
 }
